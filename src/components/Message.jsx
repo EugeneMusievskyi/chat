284230c08c69d9ef87aeefd1adbbd1b3afe89464
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-07-22 16:07:58 +0300

    Time added to message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from 'moment';
 
 class Message extends React.Component {
     constructor(props) {
@@ -7,8 +8,14 @@ class Message extends React.Component {
     }
 
     render() {
+        const createdAt = moment(this.props.message.createdAt).utc();
+        const time = createdAt !== undefined ? createdAt.format("HH:mm") : null;
+
         return (
             <div className="ui message">
+              <div className="createdAt">
+                  {time}
+              </div>
               <img className="ui avatar image tiny rounded" src={this.props.message.avatar} />
               <div className="content">
                   {this.state.message.text}
