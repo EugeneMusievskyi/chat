8a114e0ef53664f197b966caf1e3bd37db770122
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-07-22 19:16:45 +0300

    Message input added

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 class MessageInput extends React.Component {
     render() {
-        return null;
+        return (
+            <div id="inputMessage" className="ui form">
+              <div className="field">
+                <textarea rows="6" placeholder="Write your message..."/>
+              </div>
+              <button className="ui primary button right floated" type="submit">
+                  Send
+              </button>
+            </div>
+        );
     }
 }
 
