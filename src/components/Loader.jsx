578fc9d9cb426601b7ad0dfc022690953aabeb80
Zhenya Musievsky <zhenya.musievsky@gmail.com>
2020-07-24 08:50:36 +0300

    loader added

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Loader = () => {
+    return (
+        <div className="ui active loader"></div>
+    );
+};
+
+export default Loader;
--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Chat from "./container/Chat";
+import Loader from "./components/Loader";
+import {func} from "prop-types";
 
 (async () => {
+    ReactDOM.render(
+        <Loader/>,
+        document.getElementById('root')
+    );
+
     const response = await fetch("https://edikdolynskyi.github.io/react_sources/messages.json");
     let initialMessages;
-    // initialMessages = await response.json();
-    initialMessages = require("./messages.json");
-    /*if (!response.ok) {
+    if (!response.ok) {
         initialMessages = await response.json();
     } else {
         initialMessages = require("./messages.json");
-    }*/
+    }
+
     ReactDOM.render(
         <Chat messages={initialMessages}/>,
         document.getElementById('root')
     );
 })();
-
 
 export const currentUser = {
   userId: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
